0d879f183aa61831aa3d3cd931875a07132a7aed
Israel Medeiros <israel.medeiros@creditcash.com.br>
2021-08-04 16:16:11 -0300

    Messages API

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\ChatApiController;
+use App\Http\Controllers\Api\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')
+->middleware(['auth:web'])
+->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/users', [UserApiController::class, 'index']);
+
+    Route::get('/messages/create', [ChatApiController::class, 'store']);
+
+});
+
+Route::get('/', function () {
+    return ['message' => 'ok'];
 });
